3af66f6ae4f88bd3b3ee21159549f905342b3bff
eedu7 <mueedahmad067@gmail.com>
2024-09-10 07:20:23 +0500

    minor: added the user router

--- crud/users.py
+++ crud/users.py
@@ -1,36 +1,50 @@
 from fastapi import HTTPException, status
-
-from models import User
 from sqlalchemy.orm import Session
 
-from utils.password import get_password_hash
+from models import User
+from utils.password import get_password_hash, verify_password
 
 
+def get_all(db: Session):
+    try:
+        return db.query(User).all()
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=str(e)
+        )
 def get_by_email(db: Session, email: str) -> User | None:
     try:
         return db.query(User).filter(User.email == email).first()
     except Exception as e:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail=f"Not able to query data. ({e})"
+            detail=f"Not able to query data. ({e})",
         )
 
-def register_user(db: Session, user: dict) -> bool:
-    exist = get_by_email(db, email=user['email'])
-    if exist:
+
+def register_user(db: Session, user_data: dict) -> bool:
+    user = get_by_email(db, email=user_data["email"])
+    if user:
         raise HTTPException(
-            status.HTTP_400_BAD_REQUEST,
-            "User with this email already exists."
+            status.HTTP_400_BAD_REQUEST, "User with this email already exists."
         )
     try:
-        user["password"] = get_password_hash(user["password"])
-        new_user = User(**user)
+        user_data["password"] = get_password_hash(user_data["password"])
+        new_user = User(**user_data)
         db.add(new_user)
         db.commit()
         return True
     except Exception as e:
         raise HTTPException(
-            status.HTTP_400_BAD_REQUEST,
-            f"Not able to register user. ({e})"
+            status.HTTP_400_BAD_REQUEST, f"Not able to register user. ({e})"
         )
 
+
+def login_user(db: Session, user_data: dict) -> bool:
+    user = get_by_email(db, email=user_data.get("email"))
+    if not user:
+        raise HTTPException(status.HTTP_404_NOT_FOUND, "User does not exist.")
+    if not verify_password(user_data.get("password"), user.password):
+        raise HTTPException(status.HTTP_401_UNAUTHORIZED, "Invalid credentials.")
+    return True
